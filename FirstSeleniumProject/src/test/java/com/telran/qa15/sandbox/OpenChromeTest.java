39e4a5d350a2e54cd85638d77c66c049f5d4e8a7
Daniel <logradum@gmail.com>
2018-09-20 20:40:59 +0300

    Lesson open browsers in Selenium
    20.9.18

---
+++ FirstSeleniumProject/src/test/java/com/telran/qa15/sandbox/OpenChromeTest.java
@@ -0,0 +1,24 @@
+package com.telran.qa15.sandbox;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class OpenChromeTest {
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+       driver = new ChromeDriver();
+
+    }
+    @Test
+    public void testOpenBrowser(){
+        driver.navigate().to("https://www.dio.org.ua/");
+    }
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ FirstSeleniumProject/src/test/java/com/telran/qa15/sandbox/Sandbox.java
@@ -0,0 +1,8 @@
+package com.telran.qa15.sandbox;
+
+public class Sandbox {
+    String s= "text";
+    public void type (String s){
+        System.out.println(s);
+    }
+}
